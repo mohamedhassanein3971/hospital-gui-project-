a7750cad3976cdaca85236cbc53fae282d854c7b
mohamedhassanein3971 <77196446+mohamedhassanein3971@users.noreply.github.com>
2021-01-09 14:44:24 +0200

    Add files via upload

---
+++ Payments.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Payments
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comSection = new System.Windows.Forms.ComboBox();
+            this.lblPri = new System.Windows.Forms.Label();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(21, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please Choose A Medical Section :";
+            // 
+            // comSection
+            // 
+            this.comSection.BackColor = System.Drawing.Color.Gainsboro;
+            this.comSection.FormattingEnabled = true;
+            this.comSection.Items.AddRange(new object[] {
+            "General Surgery Section.",
+            "Orthopaedic Section.",
+            "Mouth & Thraot Section.",
+            "Ear Section.",
+            "Eye Section.",
+            "Nose Section."});
+            this.comSection.Location = new System.Drawing.Point(457, 81);
+            this.comSection.Name = "comSection";
+            this.comSection.Size = new System.Drawing.Size(236, 31);
+            this.comSection.TabIndex = 3;
+            this.comSection.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblPri
+            // 
+            this.lblPri.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPri.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPri.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblPri.Location = new System.Drawing.Point(457, 171);
+            this.lblPri.Name = "lblPri";
+            this.lblPri.Size = new System.Drawing.Size(236, 54);
+            this.lblPri.TabIndex = 4;
+            this.lblPri.Click += new System.EventHandler(this.lblPri_Click);
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.Lime;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(26, 171);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(165, 54);
+            this.iconButton1.TabIndex = 5;
+            this.iconButton1.Text = "Price ";
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.BackColor = System.Drawing.Color.Red;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton2.IconColor = System.Drawing.Color.Black;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.Location = new System.Drawing.Point(26, 253);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(165, 54);
+            this.iconButton2.TabIndex = 6;
+            this.iconButton2.Text = "Clear";
+            this.iconButton2.UseVisualStyleBackColor = false;
+            this.iconButton2.Click += new System.EventHandler(this.iconButton2_Click);
+            // 
+            // Payments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.Controls.Add(this.iconButton2);
+            this.Controls.Add(this.iconButton1);
+            this.Controls.Add(this.lblPri);
+            this.Controls.Add(this.comSection);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Payments";
+            this.Size = new System.Drawing.Size(1067, 685);
+            this.Load += new System.EventHandler(this.Payments_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comSection;
+        private System.Windows.Forms.Label lblPri;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton iconButton2;
+    }
+}
---
+++ Payments.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Payments : UserControl
+    {
+        public Payments()
+        {
+            InitializeComponent();
+        }
+
+        private void Payments_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void lblPri_Click(object sender, EventArgs e)
+        {
+            if (comSection.Text.ToLower() == "Ear Section.".ToLower())
+            {
+                lblPri.Text = "30$";
+            }
+            else if (comSection.Text.ToLower() == "Eye Section.".ToLower())
+            {
+                lblPri.Text = "50$";
+            }
+            else if (comSection.Text.ToLower() == "Nose Section.".ToLower())
+            {
+                lblPri.Text = "40$";
+            }
+            else if (comSection.Text.ToLower() == "Mouth & Thraot Section.".ToLower())
+            {
+                lblPri.Text = "50$";
+            }
+            else if (comSection.Text.ToLower() == "General Surgery Section.".ToLower())
+            {
+                lblPri.Text = "2000$";
+            }
+            else if (comSection.Text.ToLower() == "Orthopaedic Section Section.".ToLower())
+            {
+                lblPri.Text = "1000$";
+            }
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            if (comSection.Text.ToLower() == "Ear Section.".ToLower())
+            {
+                lblPri.Text = "30$";
+            }
+            else if (comSection.Text.ToLower() == "Eye Section.".ToLower())
+            {
+                lblPri.Text = "50$";
+            }
+            else if (comSection.Text.ToLower() == "Nose Section.".ToLower())
+            {
+                lblPri.Text = "40$";
+            }
+            else if (comSection.Text.ToLower() == "Mouth & Thraot Section.".ToLower())
+            {
+                lblPri.Text = "50$";
+            }
+            else if (comSection.Text.ToLower() == "General Surgery Section.".ToLower())
+            {
+                lblPri.Text = "2000$";
+            }
+            else if (comSection.Text.ToLower() == "Orthopaedic Section.".ToLower())
+            {
+                lblPri.Text = "1000$";
+            }
+        }
+
+        private void iconButton2_Click(object sender, EventArgs e)
+        {
+            lblPri.Text = "";
+        }
+    }
+}
+
